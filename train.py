0649b4e3be1269eeccf526c87b175eee82738ed9
Murilo Menezes Mendonça <murilommen@gmail.com>
2020-10-18 19:31:12 +0000

    ran experiments with HyperDrive

--- train.py
+++ train.py
@@ -9,16 +9,8 @@ from sklearn.preprocessing import OneHotEncoder
 import pandas as pd
 from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
+from azureml.core.dataset import Dataset
 
-data_path="https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
-
-ds = Dataset.Tabular.from_delimited_files(data_path)
-
-x, y = clean_data(ds)
-
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
-
-run = Run.get_context()
 
 def clean_data(data):
     # Dict for cleaning data
@@ -47,7 +39,7 @@ def clean_data(data):
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
     
     return x_df, y_df
-    
+
 
 def main():
     # Add arguments to script
@@ -67,4 +59,14 @@ def main():
     run.log("Accuracy", np.float(accuracy))
 
 if __name__ == '__main__':
+    data_path="https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+
+    ds = Dataset.Tabular.from_delimited_files(data_path)
+
+    x, y = clean_data(ds)
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
+
+    run = Run.get_context()
+    
     main()
